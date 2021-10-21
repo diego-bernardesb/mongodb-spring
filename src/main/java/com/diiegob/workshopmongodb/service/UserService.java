679b6ffc7ee0f784d8129ec7bad71007f651f048
diego-bernardesb <diegobbabo@gmail.com>
2021-10-21 15:41:34 -0300

    Obtendo usuario por id

--- src/main/java/com/diiegob/workshopmongodb/service/UserService.java
+++ src/main/java/com/diiegob/workshopmongodb/service/UserService.java
@@ -1,12 +1,14 @@
 package com.diiegob.workshopmongodb.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diiegob.workshopmongodb.domain.User;
 import com.diiegob.workshopmongodb.repository.UserRepository;
+import com.diiegob.workshopmongodb.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
 }
