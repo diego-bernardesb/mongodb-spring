d75dae3a9d2f3f962730f5ddc036ec4d25c87999
diego-bernardesb <diegobbabo@gmail.com>
2021-10-22 11:25:24 -0300

    Operação de DELETE inserida

--- src/main/java/com/diiegob/workshopmongodb/service/UserService.java
+++ src/main/java/com/diiegob/workshopmongodb/service/UserService.java
@@ -16,21 +16,27 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
+
+	public void delete(String id) {
+		findById(id);//tratar caso o id seja invalido
+		repo.deleteById(id);
 	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
